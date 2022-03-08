47e18aa35a15732a6a779df98b99fa76ae03daaf
khushbookum <khushbookri4375@gmail.com>
2022-03-08 14:03:43 +0530

    middleware-2

---
+++ src/controllers/userController.js
@@ -0,0 +1,12 @@
+const UserModel = require('../models/userSchema')
+
+const createUser = async function (req, res){
+
+    let user = await UserModel.create(req.body)
+    res.send({user : user})
+
+}
+
+
+
+module.exports.createUser = createUser
---
+++ src/routes/route.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const UserController= require('../controllers/userController')
+const ProductController= require('../controllers/productController')
+const HeaderMiddleware = require('../middleware/headerMiddleware')
+const OrderController = require('../controllers/orderController.js')
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+
+router.post("/createUser", HeaderMiddleware.headerValidation, UserController.createUser )
+
+// router.get("/getUsers", UserController.getUsersData)
+
+router.post("/createProduct", ProductController.createProduct  )
+
+// router.get("/getAllProducts", ProductController.getAllProducts)
+
+router.post("/createOrder", HeaderMiddleware.headerValidation, OrderController.createOrder )
+// router.post("/updateBooks", BookController.updateBooks)
+// router.post("/deleteBooks", BookController.deleteBooks)
+
+
+
+module.exports = router;
